0df9de8798ccf5ca63fab46cc4078a7c594b156c
Unknown <dadinardo@gmail.com>
2018-07-09 10:00:01 -0400

    Issues-#2 WebAPI StructureMap and further implementation

---
+++ ThePonyBookApi/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using ThePonyBookLibraries.Repositories.EF;
+using ThePonyBookLibraries.Services.Interfaces;
+
+namespace ThePonyBookApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("getuser")]
+        public AspNetUser GetUserByEmail(string email)
+        {
+            var user = _userService.GetUserByEmail(email);
+            return user;
+        }
+    }
+}
---
+++ ThePonyBookApi/DependencyResolution/StructureMapScopeModule.cs
@@ -0,0 +1,24 @@
+namespace ThePonyBookApi.DependencyResolution {
+    using System.Web;
+
+    using ThePonyBookApi.App_Start;
+
+    using StructureMap.Web.Pipeline;
+
+    public class StructureMapScopeModule : IHttpModule {
+        #region Public Methods and Operators
+
+        public void Dispose() {
+        }
+
+        public void Init(HttpApplication context) {
+            context.BeginRequest += (sender, e) => StructuremapMvc.StructureMapDependencyScope.CreateNestedContainer();
+            context.EndRequest += (sender, e) => {
+                HttpContextLifecycle.DisposeAndClearAll();
+                StructuremapMvc.StructureMapDependencyScope.DisposeNestedContainer();
+            };
+        }
+
+        #endregion
+    }
+}
